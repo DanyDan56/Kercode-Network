c4e4a454ebd812f40c9165f0771d69775ebca711
DanyDan56 <dgd.contact@gmail.com>
2022-05-17 19:25:32 +0200

    Amélioration de la navigation entre le frontend et backend (QoL)

--- app/views/front/modules/navbar.php
+++ app/views/front/modules/navbar.php
@@ -4,9 +4,15 @@
     <ul class="navbar theme-d2 font-large">
         <!-- Navbar for tablet & desktop display -->
         <li class="hide-small">
-            <a href="index.php" class="theme-d4 padding-large">
-                <i class="fa fa-home margin-right"></i>Kercode Network
-            </a>
+            <?php if ($user->isAdmin()): ?>
+                <a href="indexadmin.php" class="theme-d4 padding-large">
+                    <i class="fa fa-chart-column margin-right"></i>Administration
+                </a>
+            <?php else: ?>
+                <a href="index.php" class="theme-d4 padding-large">
+                    <i class="fa fa-home margin-right"></i>Kercode Network
+                </a>
+            <?php endif; ?>
         </li>
         <li class="">
             <a href="index.php" class="padding-large hover-white" title="Actualités">
@@ -14,24 +20,24 @@
             </a>
         </li>
         <li class="">
-            <a href="#" class="padding-large hover-white" title="Amis">
+            <a href="#" class="padding-large hover-white" title="Amis (WIP)">
                 <i class="fa fa-user"></i>
             </a>
         </li>
         <li class="">
-            <a href="#" class="padding-large hover-white" title="Messages">
+            <a href="#" class="padding-large hover-white" title="Messages (WIP)">
                 <i class="fa fa-envelope"></i>
             </a>
         </li>
         <li class="dropdown-hover">
-            <a href="#" class="padding-large hover-white" title="Notifications">
+            <a href="#" class="padding-large hover-white" title="Notifications (WIP)">
                 <i class="fa fa-bell"></i>
                 <span class="badge float-right font-small green">3</span>
             </a>
             <div class="dropdown-content dropdown-anim white card-4">
                 <a href="#">1 nouvelle requête d'ami</a>
                 <a href="#">Jean Dupont a posté sur votre mur</a>
-                <a href="#">Céline aime votre publication</a>
+                <a href="#">Daniel aime votre publication</a>
             </div>
         </li>
         <li class="right dropdown-hover">
